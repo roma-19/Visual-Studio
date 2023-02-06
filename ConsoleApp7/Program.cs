14a4212863d69ef8d1b8f42a1ea8979d6b27c29c
roman <romaroznovan69@gmail.com>
2023-02-06 21:57:49 +0200

    add ConsoleApp

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1.
+
+            //int[] mas = new int[8];
+            //for(int i = 0; i < 8; i++)
+            //{
+            //    Console.Write($"mas[{i}] = ");
+            //    mas[i] = int.Parse(Console.ReadLine());
+            //}
+            //Console.ReadKey();
+
+            //Задание 2.
+
+            //int[] mas = new int[8] { 1, -1, 2, -2, 3, -3, 4, -4 };
+            //for(int i = 0; i < 8; i++)
+            //{
+            //    if(mas[i] > 0)
+            //    {
+            //        Console.WriteLine(mas[i]);
+            //    }
+            //}
+            //Console.ReadKey();
+
+            //Задание 3.
+
+            //int[] mas = new int[] { -7, 10, 5, 16, -4, 18 };
+            //for (int i = 5; i >= 0; i--)
+            //{
+            //    Console.WriteLine($"{mas[i]}");
+            //}
+            //Console.ReadKey();
+
+            //Задание 4.
+
+            //Random ran = new Random();
+            //int[] mas = new int[12];
+            //for (int i = 0; i < 12; i++)
+            //{
+            //    mas[i] = ran.Next(163, 191);
+            //}
+            //foreach (int elem in mas)
+            //{
+            //    Console.WriteLine($"Рост человека = {elem} см");
+            //}
+            //Console.ReadKey();
+        }
+    }
+}
